df88ce3b8b966470919276a8bac8218265185f88
Arturo Muela <eltorturo93@gmail.com>
2022-05-30 18:38:30 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+
+import { StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+};
+
+export default function App() {
+  return (
+    
+      <ScrollView>
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+      <Text style={{ fontSize: 50 }}>hello</Text>
+      <Image source={logo} />
+
+    </ScrollView>
+   
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
